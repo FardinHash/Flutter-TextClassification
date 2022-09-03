79f39392460e9ac3885eb720e90f24e84c68d2f0
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-03 23:56:24 +0600

    Update tflite-converter.py

--- tflite-converter.py
+++ tflite-converter.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)  #converter for the model
 tflite_model = converter.convert()
 
-with open("model.tflite", 'wb') as f:
+with open("model.tflite", 'wb') as f:  
   f.write(tflite_model)
